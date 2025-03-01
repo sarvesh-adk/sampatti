918d907af76bbcc8f540de3b8ba2209edd0a5a2e
spam-prakash <dev.prakash.spam@gmail.com>
2025-03-02 01:27:44 +0530

    cors3

--- backend/index.js
+++ backend/index.js
@@ -11,15 +11,14 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.options('*', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://sampatti.vercel.app')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Access-Control-Allow-Origin')
-  res.setHeader('Access-Control-Allow-Credentials', 'true')
-  res.sendStatus(200)
-})
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'auth-token', 'Access-Control-Allow-Origin'],
+  credentials: true
+}
 
-// app.use(cors(corsOptions))
+app.use(cors(corsOptions))
 
 app.use('/api/auth', require('./api/auth'))
 app.use('/api/financialGoals', require('./api/financialGoals')) // Ensure this line is present
