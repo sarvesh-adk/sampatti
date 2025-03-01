27a4b1852cda88222c4fc150bbe6bef57f20af68
spam-prakash <dev.prakash.spam@gmail.com>
2025-03-01 13:13:51 +0530

    Model, database connect

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const connectToMongo = require('./db')
+
+connectToMongo()
+const cors = require('cors')
+
+const PORT = 8000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'auth-token', 'Access-Control-Allow-Origin'],
+  credentials: true
+}
+
+app.use(cors(corsOptions))
+
+// app.use('/api/auth', require('./api/auth'))
+
+app.get('/', (req, res) => {
+  res.send('Backend Route for Sampatti')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on  http://localhost:${PORT}`)
+})
---
+++ backend/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    require: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    require: true
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  name: {
+    type: String,
+    require: true
+  },
+  age: {
+    type: Number,
+    require: false
+  }
+})
+
+const User = mongoose.modelNames('user', UserSchema)
+User.createIndexes()
+
+module.exports = User
